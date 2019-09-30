23cbc996769cffb0ba54914304b14569c46dac0c
PratthamArora <arora.prattham@gmail.com>
2019-09-30 13:34:09 +0530

    setup camera view from lib added bottomSheet behaviour

--- app/src/main/java/com/prattham/facedetectapp/MainActivity.kt
+++ app/src/main/java/com/prattham/facedetectapp/MainActivity.kt
@@ -1,35 +1,52 @@
 package com.prattham.facedetectapp
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.otaliastudios.cameraview.controls.Facing
+import com.otaliastudios.cameraview.frame.Frame
+import com.otaliastudios.cameraview.frame.FrameProcessor
+import com.prattham.facedetectapp.model.FaceDetectionModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.bottom_sheet.*
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), FrameProcessor {
+
+
+    private val cameraFacing = Facing.FRONT
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<*>
+    private lateinit var faceDetectionModels: ArrayList<FaceDetectionModel>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        faceDetectionModels = ArrayList()
+        bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet)
+
+        //imageView = findViewById(R.id.face_detection_image_view)
+        //faceDetectionCameraView = findViewById(R.id.face_detection_camera_view)
+
+        //setup camera view from lib
+        face_detection_camera_view.facing = cameraFacing
+        face_detection_camera_view.setLifecycleOwner(this)
+        face_detection_camera_view.addFrameProcessor(this)
+
+        //bottomSheet behaviour
+        bottom_sheet_button.setOnClickListener {
+            if (bottomSheetBehavior.state != BottomSheetBehavior.STATE_EXPANDED)
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            else
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
 
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.menu_main, menu)
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        return when (item.itemId) {
-//            R.id.action_settings -> true
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
+    override fun process(frame: Frame) {
+
+    }
 }
